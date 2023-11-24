597a3149194afcdc5c53c020ef7b9c9b6b136aae
bbslzy <783398394@qq.com>
2023-11-24 16:52:29 +0800

    updated.

--- lib/pages/full_video_page.dart
+++ lib/pages/full_video_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class FullVideoPage extends StatefulWidget {
@@ -27,28 +28,57 @@ class _FullVideoPageState extends State<FullVideoPage> {
       ..initialize().then((_) {
         setState(() {});
       });
+    _controller.addListener(() {
+      if (_controller.value.position == _controller.value.duration) {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // 恢复系统 UI 模式
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: []);
+
+    // 释放视频控制器资源
+    _controller.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    // 设置全屏
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: []);
+
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        // 判断视频是否已加载并准备好播放
-        child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : const Text("Error"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying ? _controller.pause() : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            // 判断视频是否已加载并准备好播放
+            _controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                : const CircularProgressIndicator(),
+            IconButton(
+              icon: Icon(_controller.value.position == _controller.value.duration
+                  ? Icons.replay
+                  : _controller.value.isPlaying
+                      ? Icons.pause
+                      : Icons.play_arrow),
+              onPressed: () {
+                setState(() {
+                  _controller.value.isPlaying ? _controller.pause() : _controller.play();
+                });
+              },
+              iconSize: 48.0,
+              color: Colors.white,
+            ),
+          ],
         ),
       ),
     );
